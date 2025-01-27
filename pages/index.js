73de8be56a1f6be3057cf8bbc3f141d7efd26f98
Emerson Machado <emachado505@gmail.com>
2025-01-27 19:48:20 -0300

    Adicionado estilização na mensagem do `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
 export default function Home() {
     return (
-        <h1>Você precisa fazer aquilo que pensa que não é capaz de fazer.</h1>
+        <div style={{ 
+            position: 'fixed', top: '0', left: '0', 
+            width:'100%',height:'100%', 
+            background: '#000', color:'#fff', fontWeight: '600', 
+            display: 'flex', alignItems: 'center', justifyContent: 'center'
+        }}>
+            <h1>Você precisa fazer aquilo que pensa que não é capaz de fazer.</h1>
+        </div>
     )
 }
