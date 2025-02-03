b5f87c0adfb0b27aa9c1a056053b376f2cb34669
Emerson Machado <eMachado505@gmail.com>
2025-02-02 21:11:31 -0300

    add database.js and .env files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import { query } from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await query("select 1 + 1;");
+  console.log(result.rows);
   response.status(200).json({message: "tudo ok"});
 }
 
